8d909f2750ae93716e78a367b4228b5b66fc875c
Timothy Keaveny <timothy.keaveny@devopsec.io>
2021-04-16 12:02:42 -0400

    ADD dynamodb PUT method obj and attach it to a JS "promise()" method

--- functions/confirm-user-signup.js
+++ functions/confirm-user-signup.js
@@ -1,15 +1,17 @@
-const DynamoDB = require('aws-sdk/clients/dynamodb'); // NOTE: if entire sdk not necessary, just "require" specific mods
-const DocumentClient = new DynamoDB.DocumentClient(); // init a new Dynamodb instance
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const DocumentClient = new DynamoDB.DocumentClient();
 const { USERS_TABLE } = process.env;
 
 module.exports.handler = async (event) => {
 	if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
-
-		// how Cognito triggers work, our Lambda's must return the event itself.
-		// if we wanted to verify the user's email, the event obj is also a suitable technique.
+		await DocumentClient.put({
+			TableName: USERS_TABLE,
+			Item: user, // TODO: we still need to create a user Object
+			ConditionExpression: 'attribute_not_exists(id)' // this conditional statement checks to see if the id already exists in dynamodb table
+		}).promise(); // using javascript "promise()" obj to return a "promise" we can "await".
 		return event;
 	}
 	else {
-		return event; // if we get any other event source, just ignore & do nothing.
+		return event;
 	}
 }
