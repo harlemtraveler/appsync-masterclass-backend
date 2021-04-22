b08f7487346eb99c96a9ce1ec5806fb951e019f7
Timothy Keaveny <timothy.keaveny@devopsec.io>
2021-04-22 10:58:56 -0400

    A commit b4 I forget...next step is to create a User Object

--- functions/confirm-user-signup.js
+++ functions/confirm-user-signup.js
@@ -4,11 +4,13 @@ const { USERS_TABLE } = process.env;
 
 module.exports.handler = async (event) => {
 	if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
+		const name = event.request.userAttributes['name'];
+
 		await DocumentClient.put({
 			TableName: USERS_TABLE,
 			Item: user, // TODO: we still need to create a user Object
 			ConditionExpression: 'attribute_not_exists(id)' // this conditional statement checks to see if the id already exists in dynamodb table
-		}).promise(); // using javascript "promise()" obj to return a "promise" we can "await".
+		}).promise(); // using the javascript sdk "promise()" to return a "promise" we can "await".
 		return event;
 	}
 	else {
