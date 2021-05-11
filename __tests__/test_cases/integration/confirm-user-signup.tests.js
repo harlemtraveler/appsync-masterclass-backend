bcab0aec9b8761bc85915e1949868d005a2ea5c7
Timothy Keaveny <timothy.keaveny@devopsec.io>
2021-05-11 10:42:00 -0400

    Create the frame of what we expect the async promise workflow to be

--- __tests__/test_cases/integration/confirm-user-signup.tests.js
+++ __tests__/test_cases/integration/confirm-user-signup.tests.js
@@ -3,6 +3,14 @@ describe('When confirmUserSignup runs', () => {
 		const { name, email } = given.a_random_user()
 		const username = ...
 
-		await when.we_invoke_confirmUserSignup(username, name, email) // should create an event
+		// this will construct an event & call the confirm-user-signup() function
+		await when.we_invoke_confirmUserSignup(username, name, email)
+
+		// we now expect the user to exist in the Users DynamoDB Table
+		// create a variable to the store the returned value of this Promise
+		const ddbUser = await then.user_exists_in_UsersTable(username);
+
+		// create an expectation test make sure it is equal to "something"
+		expect(ddbUser).toEqual({});
 	})
 })
