bbcf681a3dc7b2d7fb8ceea4c253d46f35f865c9
Timothy Keaveny <timothy.keaveny@devopsec.io>
2021-05-11 10:50:52 -0400

    Mulling over the modules that we require for our test function

--- __tests__/test_cases/integration/confirm-user-signup.tests.js
+++ __tests__/test_cases/integration/confirm-user-signup.tests.js
@@ -1,16 +1,20 @@
+// these are module we intend to create and require into the file
+const given = require();
+const when = require();
+const then = require();
+// [+] we already installed the Chance pkg into our project, we just need to require() it for use
+const chance = require('chance').Chance();
+
 describe('When confirmUserSignup runs', () => {
 	it("The user's profile should be saved in DynamoDB", async () => {
-		const { name, email } = given.a_random_user()
-		const username = ...
+		const { name, email } = given.a_random_user();
+		// [+] we used the "Chance" npm pkg to generate a random user id
+		const username = chance.guid();
 
-		// this will construct an event & call the confirm-user-signup() function
 		await when.we_invoke_confirmUserSignup(username, name, email)
 
-		// we now expect the user to exist in the Users DynamoDB Table
-		// create a variable to the store the returned value of this Promise
 		const ddbUser = await then.user_exists_in_UsersTable(username);
 
-		// create an expectation test make sure it is equal to "something"
 		expect(ddbUser).toEqual({});
 	})
 })
